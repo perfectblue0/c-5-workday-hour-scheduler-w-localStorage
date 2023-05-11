b065ef15bb7dbdc58bc0021643c826863fc5064f
Paola_G <perfectblue@live.com>
2023-05-11 12:28:57 -0700

    added global variable that will be used all over js, added function that will load data from localStorage if there is any and otherwise will default object so one can store data localStorage, added function that grabs, modifies, and stores data in object, added event listener for saveBtn click that will call aforementioned function, and edited comments

--- scipt.js
+++ scipt.js
@@ -1,3 +1,5 @@
+var eventsData;
+
 // setHColors has bugs
 
 function setHColors() {
@@ -15,21 +17,41 @@ function setHColors() {
 }
 
 
-// todo: load stored data from localStorage
+// will check if there's anything in localStorage, if so will load stored data and if not will default object below to be able to store data
 function loadStoredData() {
-
+    eventsData = JSON.parse(localStorage.getItem("calendarEvents"));
+    if (!eventsData) {
+        eventsData = {
+            hour9: "",
+            hour10: "",
+            hour11: "", 
+            hour12: "", 
+            hour13: "", 
+            hour14: "", 
+            hour15: "",
+            hour16: "", 
+            hour17: ""   
+        };
+    }
 }
 
-function handleSaveClick(e) {
-    var saveButton = $(e.target);
-    // todo: store this hour's data in localStorage
+// this will grab data from html, modify, and store it in data object
+function handleSaveClick(event) {
+    var hourBlock = $(event.target).parent();
+    var value = hourBlock.children("textarea").val();
+    var hour = hourBlock.attr('id').split("-")[1];
+
+    eventsData["hour" + hour] = value;
+
+    localStorage.setItem("calendarEvents", JSON.stringify(eventsData));
 }
 
-// this will display stored data and display the colors
+
+// when user clicks a saveBtn (button), will call function
+$('.saveBtn').on('click', handleSaveClick);
+
+// this will display stored data and display the hour colors
 $(function() {
     loadStoredData();
     setHColors();
 });
-
-$('.saveBtn').on('click', handleSaveClick);
-
